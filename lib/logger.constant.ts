c6e6688bc7d11a8e58697eb7b5ed3b6d7996110a
Arferik <610231551@qq.com>
2023-05-13 17:48:23 +0800

    修复结构ddboot/config 键值匹配问题

--- lib/logger.constant.ts
+++ lib/logger.constant.ts
@@ -1,6 +1,6 @@
 export const LOG_PROVIDER = 'LOG_PROVIDER';
 export const LOG_CONFIG_OPTIONS = 'LOG_CONFIG_OPTIONS';
-export const LOG_CONFIG_KEY = 'LOG_CONFIG_KEY';
+export const LOG_CONFIG_KEY = 'log';
 export const LOG_METADATA_CONFIG = 'LOG_METADATA_CONFIG';
 export const LOG_METADATA = 'LOG_METADATA';
 export const CONFIG = 'CONFIG';
